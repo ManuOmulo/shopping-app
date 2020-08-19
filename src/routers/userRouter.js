15df0a0c0f25e90429f85a73c7586063bbfcc84c
ManuOmulo <omuloemmanuel@gmail.com>
2020-08-19 16:35:09 +0300

    encrypted passwords

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/userModel");
 
@@ -28,7 +29,7 @@ router.post("/login", async (req, res) => {
       return res.status(404).send();
     }
 
-    if (user.password !== req.body.password) {
+    if (!bcrypt.compare(user.password, req.body.password)) {
       return res.status(400).send("Email or password is incorrect");
     } else {
       res.status(200).send(user);
